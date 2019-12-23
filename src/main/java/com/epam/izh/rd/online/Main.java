aa0b4866dde58679dc6abf34020a4ce9b69eccd9
Yaro4Java <kozlov_yv@mail.ru>
2019-12-23 23:04:16 +0300

    experimenting with classes within the main() method

--- src/main/java/com/epam/izh/rd/online/Main.java
+++ src/main/java/com/epam/izh/rd/online/Main.java
@@ -1,8 +1,22 @@
 package com.epam.izh.rd.online;
 
+import com.epam.izh.rd.online.entity.Author;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        {/* SECTION FOR DEBUGGING CLASS Author */
+
+            Author author1 = new Author();
+
+            author1.setName("Yaroslav");
+            author1.setCountry("Russia");
+
+            System.out.println(author1.toString());
+            System.out.println("\nHash code is " + author1.hashCode());
+
+        }/* END OF DEBUGGING CLASS Author */
 
     }
 
